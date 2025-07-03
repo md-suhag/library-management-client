08a1afdc807a05221f962c14a8e98dbd628a9224
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-03 22:24:13 +0600

    update: api folder placement

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { baseApi } from "./features/api/baseApi";
+import { baseApi } from "./api/baseApi";
 
 export const store = configureStore({
   reducer: {
