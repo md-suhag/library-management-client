6ddde5bf62367f8e878229fd4c4b14a878852c32
md-suhag <abdussalamsuhag104@gmail.com>
2025-07-03 08:20:02 +0600

    add: books table

---
+++ src/components/module/book/Columns.tsx
@@ -0,0 +1,67 @@
+import type { IBook } from "@/types";
+import { type ColumnDef } from "@tanstack/react-table";
+
+import { MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export const columns: ColumnDef<IBook>[] = [
+  {
+    accessorKey: "title",
+    header: "Title",
+  },
+  {
+    accessorKey: "author",
+    header: "Author",
+  },
+
+  {
+    accessorKey: "genre",
+    header: "Genre",
+  },
+  {
+    accessorKey: "isbn",
+    header: "ISBN",
+  },
+
+  {
+    accessorKey: "copies",
+    header: "Copies",
+  },
+  {
+    accessorKey: "available",
+    header: "Availability",
+  },
+  {
+    id: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+      const book = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Edit book</DropdownMenuItem>
+            <DropdownMenuItem>Delete book</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Borrow Book</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- src/pages/books/BooksPage.tsx
+++ src/pages/books/BooksPage.tsx
@@ -1,22 +1,19 @@
+import { columns } from "@/components/module/book/Columns";
+import { DataTable } from "@/components/module/book/DataTable";
 import { useGetAllBooksQuery } from "@/redux/features/book/bookApi";
 import type { IBook } from "@/types";
-import React from "react";
+import React, { useEffect } from "react";
 
 const BooksPage = () => {
   const { data, isLoading, isError } = useGetAllBooksQuery(undefined, {
     refetchOnFocus: true,
   });
-  console.log(data);
+
   return (
     <div>
-      {data?.success &&
-        data.data?.map((book: IBook) => (
-          <div key={book._id} className="mb-4">
-            <h2>{book.title}</h2>
-            <p>{book.author}</p>
-            <p>{book.description}</p>
-          </div>
-        ))}
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Error loading books.</p>}
+      {data?.success && <DataTable columns={columns} data={data.data} />}
     </div>
   );
 };
